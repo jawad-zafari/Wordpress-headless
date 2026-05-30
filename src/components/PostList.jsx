0f9196daff188fc5b335651ac32942d9f59112dc
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 19:15:07 +0200

    Récupération des données de WordPress à chaque changement de numéro de page

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,6 +7,24 @@ function PostList(props) {
   const [totalPages, setTotalPages] = useState(1);
   const [expandedPostId, setExpandedPostId] = useState(null);
 
+  
+  useEffect(() => {
+    const url = props.apiUrl + '/posts?page=' + currentPage + '&per_page=1';
+    
+    fetch(url)
+      .then((response) => {
+        const total = response.headers.get('X-WP-TotalPages');
+        if (total !== null) {
+          setTotalPages(Number(total));
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+      });
+  }, [props.apiUrl, currentPage]);
+
+ 
 }
 
 export default PostList;
