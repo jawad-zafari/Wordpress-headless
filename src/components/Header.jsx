6d6d0aaf127726ff693b214b0e13ed5f69ae0034
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 18:36:17 +0200

    Changement de conditions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,25 @@ import React from 'react';
 function Header(props) {
   let title = "Derniers Articles du Blog";
   
-  if (props.currentView !== 'articles') {
+  if (props.currentView == 'home') {
     title = 'Bienvenue sur mon blog';
+  }
+  else if (props.currentView == 'about') {
+    title = 'À propos de blog';
   }
 
   return (
-<div></div>
+    <header className="App-header">
+      <nav className="top-nav">
+        <div className="logo">Mon Blog Headless</div>
+        <ul>
+          <li onClick={() => props.setCurrentView('home')}>Accueil</li>
+          <li onClick={() => props.setCurrentView('articles')}>Articles</li>
+          <li onClick={() => props.setCurrentView('about')}>À propos</li>
+        </ul>
+      </nav>
+      <h1>{title}</h1>
+    </header>
   );
 }
 
