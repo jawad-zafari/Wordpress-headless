32f81dcbd26c4ddaf0b74f2602b1d9ca0952f4c2
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 18:55:37 +0200

    Bouton Fermer

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,7 +4,21 @@ function PostCard(props) {
   const post = props.post;
   
   return (
-    
+    <article className="post-card">
+      <h2>{post.title.rendered}</h2>
+      
+      {props.isExpanded === true && (
+        <div>
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+          <button onClick={() => props.setExpandedPostId(null)}>
+            Fermer l'article
+          </button>
+        </div>
+      )}
+
+     
+    </article>
+  );
 }
 
 export default PostCard;
