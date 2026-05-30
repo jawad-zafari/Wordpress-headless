666087340574fb931d19e8d99d06b26ce20aefde
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 18:33:09 +0200

    title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 function Header(props) {
   let title = "Derniers Articles du Blog";
   
- 
+  if (props.currentView !== 'articles') {
+    title = 'Bienvenue sur mon blog';
+  }
 
   return (
-    <diV></diV>
+<div></div>
   );
 }
 
