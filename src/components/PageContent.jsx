d44a4eb4dd7b3a2767b8e95b8dd9d6d04942e0cd
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 18:40:34 +0200

    Récupérer les données du serveur

---
+++ src/components/PageContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PageContent(props) {
+  if (props.pageData === null) {
+    return null;
+  }
+
+  return (
+    <div></div>
+  );
+}
+
+export default PageContent;
