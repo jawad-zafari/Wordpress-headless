2a6e82d4c1e557cf732779af51c24575c51a0591
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 16:59:19 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+ return(
+    <div className="App">
+      <header>
+        <h1>Mon Blog Headless</h1>
+      </header>
+      <main>
+        <p>Bienvenue sur mon blog !</p>
+      </main>
+    </div>
+ );
+}
+
+export default App;
