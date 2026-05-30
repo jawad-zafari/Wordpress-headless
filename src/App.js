fed06b0e97ab3360e0d4bcbba204e901e4d56cbc
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 18:22:52 +0200

    Afficher le contenu de la page lorsqu'on clique dessus

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ import PostList from './components/PostList';
 import PageContent from './components/PageContent';
 
 function App() {
-  const [currentView, setCurrentView] = useState('articles');
-  const [pageData, setPageData] = useState(null);
+  const [currentView, setCurrentView] = useState('accueil'); 
+  const [pageData, setPageData] = useState(null); 
 
   const API_URL = process.env.REACT_APP_WP_API;
 
- 
+
+  useEffect(() => {
+    if (currentView !== 'articles') {
+      
+      let slug = 'accueil';
+      if (currentView === 'about') {
+        slug = 'a-propos';
+      }
+
+
+      const url = API_URL + '/pages?slug=' + slug;
+
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          if (data.length > 0) {
+            setPageData(data[0]); 
+
+          }
+        });
+    }
+  }, [currentView, API_URL]);
+
+
+  let sendingTitle = '';
+  if (pageData !== null) {
+    sendingTitle = pageData.title.rendered;
+  }
+
+  
 }
 
 export default App;
