51cbbd7202f17f25666226690361763d4a18489b
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 19:04:11 +0200

    Définition des states requis

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,3 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import PostCard from './PostCard';
 
+function PostList(props) {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [expandedPostId, setExpandedPostId] = useState(null);
+
+}
+
+export default PostList;
