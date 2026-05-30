701881450debdb8473117724a6a5291ef5d8e399
jawad zafari <jawad.zafari@laplateforme.io>
2026-05-30 17:56:11 +0200

    Création des constantes requises

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import PostList from './components/PostList';
 import PageContent from './components/PageContent';
 
 function App() {
-  
+  const [currentView, setCurrentView] = useState('articles');
+  const [pageData, setPageData] = useState(null);
+
+  const API_URL = process.env.REACT_APP_WP_API;
+
+ 
 }
 
 export default App;
